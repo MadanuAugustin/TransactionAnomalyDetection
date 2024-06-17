78021eca473f66f4ee8098397dd2dc087a9708dc
augustin <augustin7766@gmail.com>
2024-06-17 21:10:06 +0530

    dataValidation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 from src.TransactionAnomalyDetection.logger_file.logger_obj import logger
 from src.TransactionAnomalyDetection.Exception.custom_exception import CustomException
 from src.TransactionAnomalyDetection.pipeline.stage_01_dataIngestion import DataIngestionTrainingPipeline
-
+from src.TransactionAnomalyDetection.pipeline.stage_02_dataValidation import DataValidationTrainingPipeline
 
 
 STAGE_NAME = 'Data Ingestion Stage'
@@ -15,5 +15,19 @@ try:
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
     logger.info(f'----------------------stage {STAGE_NAME} completed ---------------------')
+except Exception as e:
+    raise CustomException(e, sys)
+
+
+
+
+
+STAGE_NAME = 'Data Validation Stage'
+
+try:
+    logger.info(f'-----------------stage {STAGE_NAME} started-----------------------')
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f'-----------------stage {STAGE_NAME} completed-----------------------')
 except Exception as e:
     raise CustomException(e, sys)
--- src/TransactionAnomalyDetection/config/configuration.py
+++ src/TransactionAnomalyDetection/config/configuration.py
@@ -2,7 +2,7 @@
 from src.TransactionAnomalyDetection.constants import *
 from src.TransactionAnomalyDetection.utils.common import read_yaml, create_directories
 from src.TransactionAnomalyDetection.logger_file.logger_obj import logger
-from src.TransactionAnomalyDetection.entity.config_entity import (DataIngestionConfig)
+from src.TransactionAnomalyDetection.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 
 class ConfigurationManager:
@@ -33,3 +33,21 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir,
+            local_data_file= config.local_data_file,
+            STATUS_FILE = config.STATUS_FILE,
+            all_schema = schema
+        )
+
+        return data_validation_config
